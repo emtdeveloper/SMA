f96b07e23ca12dfd700ab470f43810e0a9cdc5c5
ARUN SELVIN KUMAR V <68742184+emtdeveloper@users.noreply.github.com>
2025-04-28 21:05:08 -0400

    Add files via upload

---
+++ utils/db.py
@@ -0,0 +1,22 @@
+import os
+import streamlit as st
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+# Load environment variables
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+
+
+def connect_to_mongo():
+    try:
+        client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000)
+        client.admin.command('ping')
+        print("✅ Mongo connected!")
+        return client
+    except Exception as e:
+        print(f"❌ Mongo connection failed: {e}")
+        st.error("Database not reachable.")
+        st.stop()
